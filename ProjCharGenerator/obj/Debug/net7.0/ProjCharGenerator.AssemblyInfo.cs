bbb1960f5a1f372afd25081971d3471eb3d34787
CectraEnota <alena.yarakhtina@mail.ru>
2025-04-29 19:00:36 +0300

    Без тестов

--- ProjCharGenerator/obj/Debug/net7.0/ProjCharGenerator.AssemblyInfo.cs
+++ ProjCharGenerator/obj/Debug/net7.0/ProjCharGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjCharGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50b150db6b7b28420ce51b47d7d7e0a53d365e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+235b7e428d90422f7865f06041713eb1444b3fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjCharGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjCharGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
