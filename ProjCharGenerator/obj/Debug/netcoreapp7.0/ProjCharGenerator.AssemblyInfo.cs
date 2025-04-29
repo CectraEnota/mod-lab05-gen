0695c73fe0091ca506657995e2a9faf66646b94c
CectraEnota <alena.yarakhtina@mail.ru>
2025-04-29 17:10:16 +0300

    Вернула обратно

--- ProjCharGenerator/obj/Debug/netcoreapp7.0/ProjCharGenerator.AssemblyInfo.cs
+++ ProjCharGenerator/obj/Debug/netcoreapp7.0/ProjCharGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjCharGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1871965ea8b0a7c0ee56601ea6df299979abc85d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7fb0bb4fc3a16396dd2451296751d214e0cc45")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjCharGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjCharGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
